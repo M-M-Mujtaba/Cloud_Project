00dabf361460053d6f71e22923ee856a950b98e8
khizar <khezarhussain1999@gmail.com>
2021-06-18 14:50:03 +0000

    backend works

--- backend/database.js
+++ backend/database.js
@@ -4,7 +4,7 @@ var con = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
-  database: "emp_att"
+  database: process.env.MYSQL_DB
 });
 
 con.connect(function(err) {
